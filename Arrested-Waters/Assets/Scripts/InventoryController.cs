a04ecbc8ded83d37d0c193f4731f1cb0751eee95
DonNinja <yngvileo@gmail.com>
2022-03-10 12:02:30 +0000

    Made basic pickup system

--- Arrested-Waters/Assets/Scripts/InventoryController.cs
+++ Arrested-Waters/Assets/Scripts/InventoryController.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class InventoryController : MonoBehaviour
-{
+public class InventoryController : MonoBehaviour {
+    public static InventoryController instance;
 
+    [SerializeField] TextMeshProUGUI wood_text;
+    int wood_amt = 0;
 
-    // Start is called before the first frame update
-    void Start() {
-        
+    private void Awake() {
+        instance = this;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        wood_text.text = wood_amt.ToString();
     }
+
+    public void IncreaseInventory(ItemController.Material mat_type) {
+        if (mat_type == ItemController.Material.Wood) {
+            wood_amt++;
+        }
+    }
+
 }
---
+++ Arrested-Waters/Assets/Scripts/ItemController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour {
+    public InventoryController inv_cont;
+
+    public enum Material { Wood, Iron, Coal };
+
+    public Material mat_type;
+
+    // Start is called before the first frame update
+    void Start() {
+        inv_cont = InventoryController.instance;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        inv_cont.IncreaseInventory(mat_type);
+        Destroy(gameObject);
+        Destroy(this);
+    }
+
+    //private void OnTriggerEnter2D(Collision2D collision) {
+    //    inv_cont.IncreaseInventory(mat_type);
+    //    Destroy(gameObject);
+    //    Destroy(this);
+    //}
+}
