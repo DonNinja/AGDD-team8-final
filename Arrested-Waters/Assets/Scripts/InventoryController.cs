96e76a3cb57bf61a261b256310813191d6a34d0a
DonNinja <yngvileo@gmail.com>
2022-03-10 11:30:51 +0000

    Added textmeshpro

--- Arrested-Waters/Assets/Scripts/InventoryController.cs
+++ Arrested-Waters/Assets/Scripts/InventoryController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
