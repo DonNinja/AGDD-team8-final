edd7d70c5806a23083f4dc82057594a1c45e48f4
DonNinja <yngvileo@gmail.com>
2022-03-12 17:32:46 +0000

    Reformatting NPCController

--- Arrested-Waters/Assets/Scripts/NPCController.cs
+++ Arrested-Waters/Assets/Scripts/NPCController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using TMPro;
 
 public class NPCController : MonoBehaviour {
@@ -11,7 +12,8 @@ public class NPCController : MonoBehaviour {
 
     [SerializeField] TextMeshProUGUI dialogue_text;
     [SerializeField] GameObject interaction_box;
-    [SerializeField] float letter_interval;
+
+    public float letter_interval_ms;
 
     float time_counter;
     int repeat_iterator = 0;
@@ -27,32 +29,32 @@ public class NPCController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (can_talk) {
-            interaction_box.SetActive(true);
             if (Input.GetKeyDown(KeyCode.E)) {
                 Talk();
             }
         }
-        else {
-            interaction_box.SetActive(false);
-        }
     }
 
     private void FixedUpdate() {
+        // Check if we're written the entire string
         if (letter_iterator < next_string_length) {
+            // Start the counter
             time_counter += Time.deltaTime;
-            if (time_counter > letter_interval) {
-                dialogue_text.text += next_text[letter_iterator];
-                time_counter = 0;
+            if (time_counter > letter_interval_ms / 1000) { // If we've reached the desired time
+                dialogue_text.text += next_text[letter_iterator]; // Add the next letter
+                time_counter = 0; // Reset counter
                 letter_iterator++;
             }
         }
     }
 
     void Talk() {
+        // Start by iterating through main dialogue
         if (main_dialogue.Count > 0) {
             next_text = main_dialogue[0];
             main_dialogue.RemoveAt(0);
         }
+        // Then iterate and repeat repeating dialogue
         else {
             if (repeat_iterator == repeating_dialogue.Count) {
                 repeat_iterator = 0;
@@ -61,6 +63,7 @@ public class NPCController : MonoBehaviour {
             repeat_iterator++;
         }
 
+        // Set everything to be ready to display
         dialogue_text.text = "";
         next_string_length = next_text.Length;
         letter_iterator = 0;
@@ -68,15 +71,17 @@ public class NPCController : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.name == "InteractionCollider") {
+            interaction_box.SetActive(true); // Show the interaction box
             can_talk = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
         if (collision.name == "InteractionCollider") {
+            interaction_box.SetActive(false); // Hide the interaction box
             can_talk = false;
-            dialogue_text.text = "";
-            next_text = "";
+            dialogue_text.text = ""; // Empty dialogue text
+            next_text = ""; // Reset next text
         }
     }
 }
