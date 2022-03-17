b035d7f38a0c4b56b85249d856c3247882f78f00
Garrett Matthew Forthofer <gf00642@gmail.com>
2022-03-17 12:46:48 +0000

    fuck this boat

--- Arrested-Waters/Assets/Scripts/BoatManager.cs
+++ Arrested-Waters/Assets/Scripts/BoatManager.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace Arrested_Waters {
 
-    public class EnterBoat : InteractableScript
+    public class BoatManager : InteractableScript
     {
         BoatController boat;
         PlayerController player;
+        BoatCameraController boatCamera;
+
+        public float minZoom = 1f;
+        public float maxZoom = 5f;
+        public float zoomStep = 0.1f;
+        public float smoothness = 0.1f;
+
         bool onBoat = false;
         bool inBoat = false;
 
@@ -24,6 +31,12 @@ namespace Arrested_Waters {
             }
         }
 
+        private void LateUpdate()
+        {
+            if (onBoat)
+                StartCoroutine(ZoomOutSmoothly());
+        }
+
         protected override void Interact()
         {
             if (!onBoat)
@@ -36,8 +49,8 @@ namespace Arrested_Waters {
         {
             Debug.Log("Enter");
             GameManager.instance.mainCamera.target = boat.transform;
-            GameManager.instance.mainCamera.GetComponent<Animator>().SetTrigger("zoomOut");
             boat.enabled = true;
+            boatCamera.enabled = true;
             player.enabled = false;
             player.transform.position = boat.playerSeat.transform.position;
             onBoat = true;
@@ -47,13 +60,23 @@ namespace Arrested_Waters {
         {
             Debug.Log("Exit");
             GameManager.instance.mainCamera.target = player.transform;
-            GameManager.instance.mainCamera.GetComponent<Animator>().SetTrigger("zoomOut");
             boat.enabled = false;
+            boatCamera.enabled = false;
             player.enabled = true;
-            player.transform.position = boat.playerSeat.transform.position;
             onBoat = false;
 
         }
 
+        private IEnumerator ZoomOutSmoothly()
+        {
+            WaitForSeconds myWait = new WaitForSeconds(smoothness);
+            Camera.main.orthographicSize = minZoom; //you could also comment this line out and just use your current zoomValue... It's just for having a start position for the zoomanimation
+            while (Camera.main.orthographicSize < maxZoom)
+            {
+                Camera.main.orthographicSize += zoomStep * boat.speedKPH; //take your players one for the velocity  
+                yield return myWait;
+            }
+            Camera.main.orthographicSize = maxZoom; //ensure it is exactly set to the maxZoom value after the animation
+        }
     }
 }
