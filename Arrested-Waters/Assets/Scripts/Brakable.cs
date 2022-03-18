46e7c6c7f138a43ff97cbc1e476889aac0548b38
Arcmazter <aronlex@gmail.com>
2022-03-18 11:37:54 +0000

    BreakBarrels

---
+++ Arrested-Waters/Assets/Scripts/Brakable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brakable : MonoBehaviour
+{
+    public GameObject particles;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Axe")
+        {
+            Instantiate(particles, collision.transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
