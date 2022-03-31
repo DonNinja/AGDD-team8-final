2bf01609a6db260c1ef9b33f6a8d2882157cb7c7
Garrett Matthew Forthofer <gf00642@gmail.com>
2022-03-31 17:08:57 +0000

    lava done

--- Arrested-Waters/Assets/Scripts/LavaScript.cs
+++ Arrested-Waters/Assets/Scripts/LavaScript.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class LavaScript : MonoBehaviour
 {
-    PlayerController PC;
+    public PlayerStats PC;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        PC.StartBurn(10);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        PC.StopBurn();
     }
 }
