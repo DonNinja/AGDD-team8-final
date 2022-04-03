965a4790cc20ae0a4b4b12b12419276e46002955
DonNinja <yngvileo@gmail.com>
2022-04-03 14:52:43 +0000

    Update InteractableScript.cs

--- Arrested-Waters/Assets/Scripts/InteractableScript.cs
+++ Arrested-Waters/Assets/Scripts/InteractableScript.cs
@@ -10,10 +10,11 @@ namespace Arrested_Waters {
         public string what_do;
         [SerializeField] protected GameObject interaction_box;
         [SerializeField] protected TextMeshProUGUI interaction_text;
+        private bool is_interacting;
 
         // Update is called once per frame
         protected virtual void Update() {
-            if (interaction_box.activeSelf) {
+            if (interaction_box.activeSelf && is_interacting) {
                 if (Input.GetKeyDown(KeyCode.E)) {
                     Interact();
                 }
@@ -27,6 +28,7 @@ namespace Arrested_Waters {
         protected virtual void OnTriggerEnter2D(Collider2D collision) {
             if (!interaction_box.activeSelf && collision.name == "InteractionCollider") {
                 interaction_box.SetActive(true); // Show the interaction box
+                is_interacting = true;
                 interaction_text.text = what_do;
             }
         }
@@ -34,6 +36,7 @@ namespace Arrested_Waters {
         protected virtual void OnTriggerExit2D(Collider2D collision) {
             if (collision.name == "InteractionCollider") {
                 interaction_box.SetActive(false); // Hide the interaction box
+                is_interacting = false;
             }
         }
     }
